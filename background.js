c95c189958ff463bcc1b3c6279a34eea6098873e
Glitch752 <xxGlitch752xx@gmail.com>
2023-02-10 17:06:40 -0600

    Added main functionality

--- background.js
+++ background.js
@@ -1,51 +1,85 @@
 let extensionID = chrome.runtime.id;
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    // Open the extension page
-    chrome.tabs.create({url: "/index.html"});
-    // Get the current tabs
-    chrome.tabs.query({}, function(tabs) {
-        let urls = [];
-        for (let i = 0; i < tabs.length; i++) {
-            // Don't save the extension page
-            if (tabs[i].url.startsWith("chrome-extension://" + extensionID) || !tabs[i].url) continue;
+    chrome.storage.sync.set({tabs: []});
 
-            urls.push(tabs[i].url);
+    bringInTabs();
+});
 
-            // Close the tab
-            chrome.tabs.remove(tabs[i].id);
-        }
-
-        getTabs(function(tabs) {
-            if(!tabs) tabs = [];
-            tabs.push(urls);
-
-            // Save the tabs
-            saveTabs(tabs, function() {
-                // Send a message to the extension page to update the tabs
-                chrome.runtime.sendMessage({type: "updateTabs", tabs: tabs});
+chrome.runtime.onStartup.addListener(function() {
+    chrome.storage.sync.get("hasRunBefore", function(data) {
+        if(!data.hasRunBefore) {
+            chrome.storage.sync.set({hasRunBefore: true});
+            startIntro();
+        } else {
+            chrome.storage.sync.get("openOnStartup", function(data) {
+                if(data.openOnStartup) {
+                    chrome.tabs.create({url: "/index.html"});
+                }
             });
-        });
+        }
     });
-});  
+});
 
 // Handle messages from the extension page
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.type == "loadTabs") {
-        let index = request.loadTabs;
-        getTabs(function(tabs) {
-            if(!tabs || !tabs[index]) return;
+    switch(request.type) {
+        case "loadTabs": {
+            let index = request.loadTabs;
+            getTabs(function(tabs) {
+                if(!tabs || !tabs[index]) return;
 
-            // Open the tabs
-            for (let i = 0; i < tabs[index].length; i++) {
-                chrome.tabs.create({url: tabs[index][i]});
-            }
-        });
-    } else if (request.type == "updateTabs") {
-        getTabs(function(tabs) {
-            // Send a message to the extension page to update the tabs
-            chrome.runtime.sendMessage({type: "updateTabs", tabs: tabs});
-        });
+                tabs = tabs[index].groups.reduce((acc, val) => acc.concat(val), []);
+
+                // Open the tabs
+                for (let i = 0; i < tabs.length; i++) {
+                    chrome.tabs.create({url: tabs[i]});
+                }
+            });
+            break;
+        }
+        case "updateTabs": {
+            getTabs(function(tabs) {
+                // Send a message to the extension page to update the tabs
+                chrome.runtime.sendMessage({type: "updateTabs", tabs: tabs});
+            });
+            break;
+        }
+        case "deleteTabGroup": {
+            let index = request.deleteTabGroup;
+            getTabs(function(tabs) {
+                if(!tabs || !tabs[index]) return;
+
+                tabs.splice(index, 1);
+
+                // Save the tabs
+                saveTabs(tabs, function() {
+                    // Send a message to the extension page to update the tabs
+                    chrome.runtime.sendMessage({type: "updateTabs", tabs: tabs});
+                });
+            });
+            break;
+        }
+        case "changeTabGroupName": {
+            let index = request.changeTabGroupName;
+            let name = request.name;
+            getTabs(function(tabs) {
+                if(!tabs || !tabs[index]) return;
+
+                tabs[index].name = name;
+
+                // Save the tabs
+                saveTabs(tabs, function() {
+                    // Send a message to the extension page to update the tabs
+                    chrome.runtime.sendMessage({type: "updateTabs", tabs: tabs});
+                });
+            });
+            break;
+        }
+        case "saveTabs": {
+            bringInTabs();
+            break;
+        }
     }
 });
 
@@ -60,5 +94,46 @@ function getTabs(callback) {
     // Get the tabs from storage
     chrome.storage.sync.get("tabs", function(data) {
         if(callback) callback(data.tabs);
+    });
+}
+
+function bringInTabs() {
+    // Get the current tabs
+    chrome.tabs.query({}, function(tabs) {
+        if(!tabs.find(tab => tab.url.startsWith("chrome-extension://" + extensionID))) {
+            // Open the extension page if it isn't already open
+            chrome.tabs.create({url: "/index.html"});
+        }
+
+        // Keys are group ids, values are an object with the properties:
+        //   tabs (an array of tab urls)
+        //   name (the name of the group)
+        //   color (the color of the group)
+
+        let urls = {}; 
+        for (let i = 0; i < tabs.length; i++) {
+            // Don't save the extension page
+            if (tabs[i].url.startsWith("chrome-extension://" + extensionID) || !tabs[i].url) continue;
+
+            urls[tabs[i].groupId] = urls[tabs[i].groupId] || {tabs: [], name: "Group " + tabs[i].groupId, color: "red"};
+            urls[tabs[i].groupId].tabs.push(tabs[i].url);
+
+            // Close the tab
+            chrome.tabs.remove(tabs[i].id);
+        }
+
+        getTabs(function(tabs) {
+            if(!tabs) tabs = [];
+            tabs.push({
+                groups: urls,
+                name: "Science"
+            });
+
+            // Save the tabs
+            saveTabs(tabs, function() {
+                // Send a message to the extension page to update the tabs
+                chrome.runtime.sendMessage({type: "updateTabs", tabs: tabs});
+            });
+        });
     });
 }
--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-function loadTabs(index) {
-    // Send a message to the background script to load the tabs
-    chrome.runtime.sendMessage({type: "loadTabs", loadTabs: index});
-}
-
 // Add a listener for messages from the background script
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.type == "updateTabs") {
-        console.log(request.tabs);
         // Update the tabs
         updateTabs(request.tabs);
     }
@@ -14,21 +8,61 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
 function updateTabs(tabs) {
     let tabsElement = document.getElementById("tabs");
+    tabsElement.innerHTML = "";
     for(let i = 0; i < tabs.length; i++) {
         let addedHTML = "";
-        addedHTML  += `<div class="tabGroup"><h1>${"Science"}</h1><div class="tabList">`;
-        for(let j = 0; j < tabs[i].length; j++) {
-            addedHTML += `<img src="chrome://favicon/${tabs[i][j]}" />`;
+        addedHTML  += `<div class="tabGroup"><input type="text" value="${tabs[i].name}" /><div class="tabList">`;
+        for(let group in tabs[i].groups) {
+            let groupData = tabs[i].groups[group];
+            addedHTML += `<div class="groupColor ${groupData.color}">`
+            for(let j = 0; j < groupData.tabs.length; j++) {
+                addedHTML += `<img src="chrome://favicon/${groupData.tabs[j]}" />`;
+            }
+            addedHTML += `</div>`;
         }
         addedHTML += `</div><div class="buttons">
-            <button class="load button" onclick="loadTabs(${i})">Load</button>
-            <button class="close button" onclick="closeTabs(${i})">X</button>
+            <button class="load button">Load</button>
+            <button class="close button">X</button>
         </div></div>`;
-        tabsElement.innerHTML += addedHTML;
+        tabsElement.appendChild(document.createRange().createContextualFragment(addedHTML));
+
+        // Add listeners to the buttons
+        let buttons = tabsElement.querySelectorAll(".buttons")[i];
+        buttons.querySelectorAll(".load")[0].addEventListener("click", function() {
+            loadTabs(i);
+        });
+        buttons.querySelectorAll(".close")[0].addEventListener("click", function() {
+            deleteTabGroup(i);
+        });
+
+        // Add a listener to the text box
+        let textBox = tabsElement.querySelectorAll("input")[i];
+        textBox.addEventListener("change", function() {
+            changeTabGroupName(i, textBox.value);
+        });
     }
+}
+
+function loadTabs(index) {
+    // Send a message to the background script to load the tabs
+    chrome.runtime.sendMessage({type: "loadTabs", loadTabs: index});
+}
+function deleteTabGroup(index) {
+    // Send a message to the background script to delete the tab group
+    chrome.runtime.sendMessage({type: "deleteTabGroup", deleteTabGroup: index});
+}
+function changeTabGroupName(index, name) {
+    // Send a message to the background script to change the tab group name
+    chrome.runtime.sendMessage({type: "changeTabGroupName", changeTabGroupName: index, name: name});
 }
 
 window.onload = function() {
     // Send a message to the background script to get the tabs
     chrome.runtime.sendMessage({type: "updateTabs"});
+
+    // Add button listeners
+    document.querySelector(".button.bring-tabs").addEventListener("click", function() {
+        // Send a message to the background script to save the tabs
+        chrome.runtime.sendMessage({type: "saveTabs"});
+    });
 }
